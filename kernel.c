99975994a7bf318c9eb0fb3d2548fd16612f2fb3
alaa-megahed <alaamogahed2014@gmail.com>
2017-04-07 15:18:19 +0200

    copy not working

--- kernel.c
+++ kernel.c
@@ -1,45 +1,21 @@
 void printString(char*);
 void readString(char*);
-void readSector(char* buffer, int sector);  
-void writeSector(char* buffer, int sector); 
+void readSector(char* buffer, int sector); 
+void writeSector(char* buffer, int sector);  
 void handleInterrupt21(int ax, int bx, int cx, int dx); 
 int readFile(char*, char*); 
+void writeFile(char* name, char* buffer, int secNum);
 int executeProgram(char* name, int segment);
 void terminate();
-// void writeFile(char* name, char* buffer, int secNum);
 void deleteFile(char* name);
-
 
 int main () 
 {
-	// /* 
-	// 	Testing Task1
-	// */ 
-	// char buffer[13312]; /*this is the maximum size of a file*/
-	// makeInterrupt21();
-	// interrupt(0x21, 3, "messag\0", buffer, 0); /*read the file into buffer*/
-	// interrupt(0x21, 0, buffer, 0, 0); /*print out the file*/
-	// while(1); /*hang up*/
-
-	// /*
-	// 	Testing Task2
-	// */
-	// makeInterrupt21();
-	// interrupt(0x21, 4, "tstprg\0", 0x2000, 0);
-	// while(1);
-
-	// /* 
-	// 	Testing Task3
-	// */
-	// makeInterrupt21();
-	// interrupt(0x21, 4, "tstpr2\0", 0x2000, 0);
-	// while(1);
-
-
-		// Testing Task4
-	
-	makeInterrupt21();	
-	interrupt(0x21, 4, "shell\0", 0x3000, 0);
+	/*
+		Testing Task4 M2
+	*/
+	makeInterrupt21();
+	interrupt(0x21, 4, "shell\0", 0x2000, 0);
 
 
 	/*  Testing Task 2 in milestone 3 */
@@ -53,21 +29,19 @@ int main ()
 
 	/* Testing Task 3 */
 
-	int i=0;
-	char buffer1[13312];
-	char buffer2[13312];
-	buffer2[0]= 'h'; 
-	buffer2[1]='e'; 
-	buffer2[2]='l';
-	buffer2[3]='l';
-	buffer2[4]= 'o';
-	for(i=5; i<13312; i++) buffer2[i]=0x0;
-		makeInterrupt21();
-	interrupt(0x21,8, "testW\0", buffer2, 1); //write file testW
-	interrupt(0x21,3, "testW\0", buffer1, 0); //read file testW
-	interrupt(0x21,0, buffer1, 0, 0); // print out contents of testW
-
-
+	// int i=0;
+	// char buffer1[13312];
+	// char buffer2[13312];
+	// buffer2[0]= 'h'; 
+	// buffer2[1]='e'; 
+	// buffer2[2]='l';
+	// buffer2[3]='l';
+	// buffer2[4]= 'o';
+	// for(i=5; i<13312; i++) buffer2[i]=0x0;
+	// makeInterrupt21();
+	// interrupt(0x21,8, "testW\0", buffer2, 1); //write file testW
+	// interrupt(0x21,3, "testW\0", buffer1, 0); //read file testW
+	// interrupt(0x21,0, buffer1, 0, 0); // print out contents of testW
 }
 
 void printString(char* s)
@@ -108,25 +82,25 @@ void readString(char* s)
 		// again write '\b' to move the cursor back one character
 		// and update the current index of the String
 		if(in == 0x8)
-			{	if(i > 0) 
-				{
-					interrupt(0x10, 0xE*256+'\b', 0, 0, 0);
-					interrupt(0x10, 0xE*256+' ', 0, 0, 0);
-					interrupt(0x10, 0xE*256+'\b', 0, 0, 0);
-					i--;
-				}
-			}
-			else
+		{	if(i > 0) 
 			{
-				s[i] = in;
-				interrupt(0x10, 0xE*256+s[i++], 0, 0, 0);
+				interrupt(0x10, 0xE*256+'\b', 0, 0, 0);
+				interrupt(0x10, 0xE*256+' ', 0, 0, 0);
+				interrupt(0x10, 0xE*256+'\b', 0, 0, 0);
+				i--;
 			}
 		}
+		else
+		{
+			s[i] = in;
+			interrupt(0x10, 0xE*256+s[i++], 0, 0, 0);
+		}
 	}
+}
 
-	void readSector(char* buffer, int sector) {
-		int AH = 2; 
-		int AL = 1; 
+void readSector(char* buffer, int sector) {
+	int AH = 2; 
+	int AL = 1; 
 	int CH = sector / 36 ;                //cylinder
 	int CL = mod(sector, 18) + 1;         //sector 
 	int DH = mod(sector / 18, 2);         //head 
@@ -167,9 +141,9 @@ void handleInterrupt21(int ax, int bx, int cx, int dx) {
 	else if(ax==7){
 		deleteFile(bx);
 	}
-	// else if(ax==8){
-	// 	writeFile(bx,cx,dx);
-	// }
+	else if(ax==8){
+		writeFile(bx,cx,dx);
+	}
 	else {
 		printString("Error");
 	}
@@ -182,14 +156,11 @@ int readFile(char* fileName, char* buffer)
 {
 	// Load the directory sector into a 512 byte character array using readSector
 	char directory[512] ;
-	char map[512]; 
 	int i = 0;
 	char fail[16];
 	readSector(directory, 2);
-	readSector(map, 1); 
 
 	// Go through the directory trying to match the file name
-
 	for(i = 0; i < 512; i += 32)
 	{
 		int found = 1; 
@@ -214,12 +185,10 @@ int readFile(char* fileName, char* buffer)
 			while(directory[k]) 
 			{
 				readSector(buffer + (count++) * 512, directory[k++]); 
-				count++; 
 			}
-			return count;
+			return 1;
 		}
 	}
-
 	// Print failure message if file not found
 	fail[0]='F';
 	fail[1]='I';
@@ -240,42 +209,9 @@ int readFile(char* fileName, char* buffer)
 	fail[16]='\0';
 	interrupt(0x21, 0, fail, 0, 0);
 	return 2;
-
 }
-/**
-	Take as input a filename and deletes that file 
-*/
-//  void deleteFile(char* fileName) {
-// 	 char[512] directory, map; 
-// 	 int found = 1; 
-// 		int j = 0;
-// 	for(i = 0; i < 512; i += 32) {
-// 		int found = 1; 
-// 		int j = 0;
-// 		for(j = 0; j < 6; j++) 
-// 		{
-// 			if(fileName[j] != directory[i + j]) 
-// 			{
-// 				found = 0; 
-// 				break; 
-// 			}
-// 			// if the file name is less than 6 characters, break
-// 			if(fileName[j] == '\0')
-// 				break;
-// 		}
-// 		if(found) 
-// 		{
-// 			directory[i] = 0x00; 
-// 			for(int k = i + 6; k < i + 32; k++) {
-// 				map[directory[k]] = 0x0; 
-// 			}
-// 		}
-// 	}
-
-//  }
 
 /*
-
 	Takes as input the name of a program and the segment where you want it to run
 */
 int executeProgram(char* name, int segment)
@@ -324,126 +260,120 @@ void writeSector(char* buffer, int sector)
 	interrupt(0x13,AH*256 + AL, buffer, CH*256 + CL, DH*256 + DL); 
 }
 
-<<<<<<< HEAD
 void deleteFile(char* name)
 {
-	=======
-	void deleteFile(char* name) {
-
-		>>>>>>> 15c2612dd6cd27035da521ed5d5188c069adca4d
-		char directory [512];
-		char map [512];
-		int i = 0; 
+	char directory [512];
+	char map [512];
+	int i = 0; 
 	//loading the map and directory
-		readSector(map,1);
-		readSector(directory,2);
+	readSector(map,1);
+	readSector(directory,2);
 
  	// Go through the directory trying to match the file name
 
-		for(; i < 512; i += 32)
+	for(; i < 512; i += 32)
+	{
+		int found = 1; 
+		int j = 0;
+		for(j = 0; j < 6; j++) 
 		{
-			int found = 1; 
-			int j = 0;
-			for(j = 0; j < 6; j++) 
+			if(name[j] != directory[i + j]) 
 			{
-				if(name[j] != directory[i + j]) 
-				{
-					found = 0; 
-					break; 
-				}
+				found = 0; 
+				break; 
+			}
 			// If the file name is less than 6 characters, break
+			if(name[j] == '\0')
+				break;
+		}
+
+		// Using the sector numbers in the directory, puting zero in map sectors corresponding to them
+		if(found) 
+			{	int k; 
+				directory[i] = 0x00; 
+				k = i + 6;
+				while(directory[k]) 
+				{
+					map[directory[k]+1] = 0x00;
+					k++;				 
+				}
+				writeSector(directory,2);
+				writeSector(map,1);
+				break;
+			}
+		}
+
+	}
+
+
+void writeFile(char* name, char* buffer, int secNum)
+{
+	char directory [512];
+	char map [512];
+	int i =0;
+	int found =0;
+	//loading the map and directory
+	readSector(map,1);
+	readSector(directory,2);
+
+    // find a free entry to write the file in the directory
+	for(i = 0; i < 512; i += 32)
+	{
+		if (directory[i]==0x00)
+		{
+			int j = 0;
+			int k = 0;
+			int l = i + 6;
+			int count = 0;
+			int done = 0;
+			int f = i + 6 + secNum;
+    		//writing the name of the file in the directory entry
+			for(j = 0; j < 6; j++) 
+			{		
+				directory[i+j] = name[j];
 				if(name[j] == '\0')
 					break;
 			}
-
-		// Using the sector numbers in the directory, puting zero in map sectors corresponding to them
-			if(found) 
-				{	int k; 
-					directory[i] = 0x00; 
-					k = i + 6;
-					while(directory[k]) 
-					{
-						map[directory[k]+1] = 0x00;
-						k++;				 
-					}
-					writeSector(directory,2);
-					writeSector(map,1);
-					break;
-				}
+			for(; j < 6; j++)
+			{
+				directory[i+j] = 0x00;
 			}
 
-		}
-
-
-		void writeFile(char* name, char* buffer, int secNum)
-		{
-
-			char directory [512];
-			char map [512];
-			int i =0;
-			int found =0;
-	//loading the map and directory
-			readSector(map,1);
-			readSector(directory,2);
-
-    // find a free entry to write the file in the directory
-			for(i = 0; i < 512; i += 32)
+			for(j = 0; j < secNum; j++)
 			{
-				if (directory[i]==0x00)
+				for (k = 0; k < 512 ; k++)
 				{
-					int j = 0;
-					int k = 0;
-					int l = i + 6;
-					int count = 0;
-					int done = 0;
-					int f = i + 6 + secNum;
-    			//writing the name of the file in the directory entry
-					for(j = 0; j < 6; j++) 
-					{		
-						directory[i+j] = name[j];
-						if(name[j] == '\0')
-							break;
-					}
-					for(; j < 6; j++)
+					if(map[k]==0x00)
 					{
-						directory[i+j] = 0x00;
+						map[k]=0xFF;
+						directory[l++] = k - 1;
+						done++;
+						writeSector(buffer + (count++) * 512, k - 1);
+						break;
 					}
-
-					for(j = 0; j < secNum; j++)
-					{
-						for (k = 0; k < 512 ; k++)
-						{
-							if(map[k]==0x00)
-							{
-								map[k]=0xFF;
-								directory[l++] = k - 1;
-								done++;
-								writeSector(buffer + (count++) * 512, k - 1);
-								break;
-							}
-						}
-						if(done < j + 1)
-						{
-							printString("There is no space in map");
-							return;
-						}
-					}
-					for(j = f; j < 32; j++)
-					{
-						directory[j] = 0x00;
-					}
-
-					writeSector(map,1);
-					writeSector(directory,2);
-					found =1;
-					break;
-
 				}
-
+				if(done < j + 1)
+				{
+					printString("There is no space in map");
+					return;
+				}
 			}
-			if(!found)
+			for(j = f; j < 32; j++)
 			{
-				printString("There is no space in directory");
-				return;
+				directory[j] = 0x00;
 			}
+
+			writeSector(map,1);
+			writeSector(directory,2);
+			found =1;
+			break;
+
 		}
+
+	}
+	if(!found)
+	{
+		printString("There is no space in directory");
+		return;
+	}
+}
--- shell.c
+++ shell.c
@@ -30,15 +30,16 @@ void split(char* line)
 		// Split the command into tokens, save them in the token array
 		while(*(line + i) != ' ') 
 		{
-			*(tmp + k) = *(line + i);
 			if(*(line + i) == '\0')
 			{
 				i--;
 				break;
 			} 
+			*(tmp + k) = *(line + i);
 			k++;
 			i++;	
 		}
+		
 		*(tmp + k) = '\0';
 		(token[j]) = tmp;
 		tmp = "";
@@ -46,7 +47,6 @@ void split(char* line)
 		i++;
 		j++;
 	}
-
 
 	// If command is view, load the desired file and print out its content
 	if(cmprstr(token[0],"view\0"))
@@ -57,19 +57,29 @@ void split(char* line)
 		interrupt(0x21, 0, buffer, 0, 0);         /*print out the file*/				
 	}
 	// If command is execute, execute the desired program
-	else if(cmprstr(token[0],"execute"))
+	else if(cmprstr(token[0],"execute\0"))
 	{
 		interrupt(0x21, 4, token[1], 0x2000, 0);	
-	} else if(cmprstr(token[0],"delete")) {
-	
-		interrupt(0x21, 7, token[1], 0, 0); 
-	
-	} else if(cmprstr(token[0]), "copy") {
-		char* buffer; 
-		interrupt(0x21, 3, token[1], buffer, 0); //read file
-		// interrupt(0x21, 8, token[2], buffer, 3); //write file 	
 	}
-	else if(cmprstr(token[0], "dir")) {
+	else if(cmprstr(token[0],"delete\0")) 
+	{
+		interrupt(0x21, 7, token[1], 0, 0); 	
+	}
+	else if(cmprstr(token[0], "copy\0")) 
+	{
+		char* buffer;
+		buffer = ""; 
+		interrupt(0x21, 0, token[0], 0, 0);
+		interrupt(0x21, 0, token[1], 0, 0);
+		interrupt(0x21, 0, token[2], 0, 0);
+		interrupt(0x21, 3, token[1], buffer, 0); //read file
+		interrupt(0x21, 0, "buffer", 0, 0);
+		interrupt(0x21, 0, buffer, 0, 0);
+		interrupt(0x21, 0, "read done", 0, 0);
+		interrupt(0x21, 8, token[2], buffer, 3); //write file 	
+	}
+	else if(cmprstr(token[0], "dir")) 
+	{
 		char dir [512] ; 
 		char fileName[6]; 
 		int i; 	
@@ -88,7 +98,6 @@ void split(char* line)
 			interrupt(0x21, 0, fileName, 0, 0); 
 		}
 	}
-	
 	// If the command written wasn't vlaid, print out BAD COMMAND
 	else
 	{
@@ -115,5 +124,4 @@ int cmprstr(char* str1,char* str2)
     if (noteq == 0 && str1[i] == '\0' && str2[i] == '\0')
          return 1;
     return 0;
-
 }
